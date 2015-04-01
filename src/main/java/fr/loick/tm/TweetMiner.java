69a8d338207d217ce936a591facfe313948cb196
q13000412 <q13000412@A-F001802-L>
2015-04-01 14:16:31 +0200

    first commit - hello world

--- src/main/java/fr/loick/tm/TweetMiner.java
+++ src/main/java/fr/loick/tm/TweetMiner.java
@@ -4,4 +4,7 @@ package fr.loick.tm;
  * Created by loick on 01/04/15.
  */
 public class TweetMiner {
+    public static void main(String[] args) {
+        System.out.println("Hello world !");
+    }
 }
