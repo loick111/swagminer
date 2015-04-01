9ace2bc0652eb268f114fddf9fa900b543db4a27
PLoic <loic.pauletto@gmail.com>
2015-04-01 17:41:21 +0200

    Begin map converter

---
+++ src/main/java/fr/loick/tm/converter/MapConverter.java
@@ -0,0 +1,44 @@
+package fr.loick.tm.converter;
+
+import java.io.*;
+import java.util.Map;
+
+/**
+ * Created by p13005682 on 01/04/15.
+ */
+public class MapConverter implements Serializable {
+    private Map<String,Integer> map;
+    BufferedWriter bw;
+
+    public MapConverter(Map<String, Integer> map, File file) {
+
+        try {
+            this.map = map;
+            bw = new BufferedWriter(new FileWriter(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void convert(){
+        try {
+
+            for (Map.Entry<String,Integer> entry : map.entrySet()){
+
+                bw.write(entry.getValue() + ";" + entry.getKey());
+                bw.newLine();
+                bw.flush();
+
+            }
+
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
